0d168cc7baba03516dba44521b4adf12acf1a5c6
Sophia <sophiaaa@llip.life>
2017-11-07 16:28:14 -0800

    express tutorial: Create a server that displays a list of users on your computer that is not you

--- express/expressTutorial.js
+++ express/expressTutorial.js
@@ -1,13 +1,31 @@
+//Create a server that displays a list of users on your computer that is not you
+
+const fileSystem = require('fs');
 const express = require('express');
 const app = express();
-const handle = (req, res) => { 
-  //req = request, res = response 
-  console.log("Print to terminal");
-  // response.send("Send response to browser");
-  // need to commented the above b/c you can only send once
-  response.send("2" + "3");
-}
-//app.use(express.static('public'));
 app.listen(3138);
+
+const handle = (req, res) => { 
+  // req = request, res = response 
+  // this is a server file, so you'll see: console.log("Print to terminal");
+  // res.send("Send response to browser"); --OR-- res.send("2" + "3");
+  // note: whenever writing send function, you can only send once
+  
+  let nameString = " ";
+  const checkMyName = (name) => {
+    if (name !== "sophiaaa") {
+      nameString = nameString + ' ' + name + '\n';
+    }
+  }
+  const directoryNames = (err, data) => {
+    data.forEach(checkMyName);
+    res.send(nameString);
+  }
+  fileSystem.readdir('/home', directoryNames);
+};
+
+//app.use(express.static('public'));
 app.get('/', handle);
+
+
 
